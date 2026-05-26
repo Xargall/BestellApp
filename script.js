8a614de66bbe93c560ad50633dee68ada4c6fcae
Mathias Mayer <mayer.mathias@proton.me>
2026-05-26 13:18:15 +0200

    feat added main structure, style nav bar and landing section, added icons, img, etc.

---
+++ script.js
@@ -0,0 +1,8 @@
+function openBurgerMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
---
+++ scripts/db.js
@@ -0,0 +1,85 @@
+const burgers = [
+  {
+    "name": "Veggie mushroom black burger",
+    "price": 16.9,
+    "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+    "quantity": 0,
+  },
+  {
+    "name": "All meat burger",
+    "price": 15.9,
+    "description":
+      "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+    "quantity": 0,
+  },
+  {
+    "name": "Beef red burger",
+    "price": 14.9,
+    "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+    "quantity": 0,
+  },
+  {
+    "name": "Big chicken burger",
+    "price": 15.9,
+    "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell Pepper",
+    "quantity": 0,
+  },
+];
+
+const pizzas = [
+  {
+    "name": "Pizza Margherita",
+    "price": 11.9,
+    "description": "Tomato Sauce, Mozzarella",
+    "quantity": 0,
+  },
+  {
+    "name": "Chorizo",
+    "price": 13.9,
+    "description": "Tomato Slices, Mozzarella, Chorizo",
+    "quantity": 0,
+  },
+  {
+    "name": "Pizza Funghi",
+    "price": 12.9,
+    "description": "Red onion, Button Mushrooms, Mozzarella",
+    "quantity": 0,
+  },
+  {
+    "name": "Quattro Formaggi with Chicken",
+    "price": 15.9,
+    "description":
+      "Chicken, mozzarella, Gorgonzola, Fontina, Parmeggiano Reggiano",
+    "quantity": 0,
+  },
+];
+
+const salads = [
+  {
+    "name": "Warm beef arugula salad",
+    "price": 16.9,
+    "description":
+      "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried tomatoes, Balsamic-Vinegar dressing",
+    "quantity": 0,
+  },
+  {
+    "name": "Mini green salad",
+    "price": 7.9,
+    "description": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+    "quantity": 0,
+  },
+  {
+    "name": "Green salad with sea food",
+    "price": 16.9,
+    "description":
+      "Mixed green, Cherry tomatoes, Red onion, Mussles, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+    "quantity": 0,
+  },
+  {
+    "name": "Vegan green salad with tofu",
+    "price": 14.9,
+    "description":
+      "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts ",
+    "quantity": 0,
+  },
+];
